e02329b4126c475a42689428df5ee82f7d3aab4a
binhpt <thebinh1992@gmail.com>
2014-09-29 21:31:35 +0700

    binhpt

--- application/modules/default/Bootstrap.php
+++ application/modules/default/Bootstrap.php
@@ -1,15 +1,16 @@
 <?php
 class Default_Bootstrap extends Zend_Application_Module_Bootstrap{
-//    protected function _initDefaultRouter(){
+    protected function _initDefaultRouter(){
 //        $route = 'login.html';
-//        $defaults = array('module'=>'admin','controller'=>'index','action'=>'index');
+//        $defaults = array('module'=>'default','controller'=>'index','action'=>'index');
+//        Add_Route_Static($route, $defaults, 'lohin');
 //        $objRoute = new Zend_Controller_Router_Route_Static($route,$defaults);
 //        
 //        $font = Zend_Controller_Front::getInstance();
 //        $fontRoute = $font->getRouter();
 //        $fontRoute->addRoute('admin',$objRoute);
-//        
-//        $route = 'demo/(\d+)/(.*)';
+        
+//        $route = 'demo/?(\d+)/?(.*)';
 //        $defaults = array('module'=>'default','controller'=>'index','action'=>'index');
 //        $map = array(1=>'id',2=>'name');
 //        $objRoute = new Zend_Controller_Router_Route_Regex($route, $defaults, $map);
@@ -17,5 +18,5 @@ class Default_Bootstrap extends Zend_Application_Module_Bootstrap{
 //        $font = Zend_Controller_Front::getInstance();
 //        $fontRoute = $font->getRouter();
 //        $fontRoute->addRoute('default',$objRoute);
-//    }
+    }
 }
--- functions.php
+++ functions.php
@@ -5,4 +5,24 @@ function __($str) {
     $translate =Zend_Registry::get('Zend_Translate');
     return $translate->_($str);
 }
-//
+//Add Route static
+function Add_Route_Static($route,$defaults,$routeName){
+     $objRoute = new Zend_Controller_Router_Route_Static($route,$defaults);
+     $font = Zend_Controller_Front::getInstance();
+     $fontRoute = $font->getRouter();
+     $fontRoute->addRoute($routeName,$objRoute);
+}
+//Add Route Regex
+function  Add_Route_Regex($route,$defaults,$map,$routeName){
+    $objRoute = new Zend_Controller_Router_Route_Regex($route, $defaults, $map);
+    $font = Zend_Controller_Front::getInstance();
+    $fontRoute = $font->getRouter();
+    $fontRoute->addRoute($routeName,$objRoute);
+}
+//Add Route RegRouteex
+function  Add_Route($route,$defaults,$reqs,$routeName){
+    $objRoute = new Zend_Controller_Router_Route_Regex($route, $defaults, $reqs);
+    $font = Zend_Controller_Front::getInstance();
+    $fontRoute = $font->getRouter();
+    $fontRoute->addRoute($routeName,$objRoute);
+}
