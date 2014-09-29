1785f37215424f5ac4271ada8f2f67bcf482891d
binhpt <thebinh1992@gmail.com>
2014-09-29 20:33:21 +0700

    binhpt

--- library/FTeam/Controller/Action.php
+++ library/FTeam/Controller/Action.php
@@ -3,7 +3,6 @@ class FTeam_Controller_Action extends Zend_Controller_Action{
 	
 	public function init(){
 		$this->loadTemplate(DEFAULT_TEMPLATE);
-                $this->changLanguages();
 	}
 	
 	protected function loadTemplate($template_path, $fileConfig = 'template.ini',$sectionConfig = 'template'){
