f428456260b8b29f811effebf356d0cb9514cc90
TrangVT <trangvt@smartosc.com>
2014-10-07 23:42:18 +0700

    TrangVT - Admin price and time module

---
+++ application/modules/admin/controllers/PricesController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *TrangVT
+ */
+class Admin_PricesController extends FTeam_Controller_AdminAction
+{
+
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function indexAction(){
+
+    }
+
+}
