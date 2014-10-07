dcf49519a666b1b5273a4e96daff2a46ac6f546f
tienln <tienln@smartosc.com>
2014-10-07 18:17:30 +0700

    catch case no router correct, redir to 404 page

--- index.php
+++ index.php
@@ -7,4 +7,12 @@ require_once 'Zend/Application.php';
 $environment = APPLICATION_ENV;
 $options = APPLICATION_PATH . '/configs/application.ini';
 $application = new Zend_Application($environment, $options);
-$application->bootstrap()->run();
+$application->bootstrap();
+
+$front = Zend_Controller_Front::getInstance();
+try {
+    $front->dispatch();
+} catch (Zend_Exception $e) {
+    header( 'HTTP/1.0 404 Not Found' );
+    echo "<div style='float:center; width:1000px; margin:0 auto;'><img src='http://www.barrywise.com/wp-content/uploads/2008/08/404-page-not-found-image-194x300.jpg'  alt='Everything is gonna be fine, please do not panic!' /></div>";
+}
