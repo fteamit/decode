37f40dc7b46080c527c216e046471bcbddf9d52f
Nguyen Tien Dung <NTienDung90gmail.com>
2014-10-07 22:45:45 +0700

    update slide ảnh trang the game

--- public/templates/default/default/html/top_content.php
+++ public/templates/default/default/html/top_content.php
@@ -38,10 +38,32 @@
         <?php endif; ?>
     <?php elseif ($this->class_body === 'the-game'): ?>
         <div id="video-banner">
-            <div class="embed-responsive embed-responsive-16by9"
-                 style="border: 1px solid #ccc">
-                <iframe class="embed-responsive-item"
-                        src="//www.youtube.com/embed/jbPW10koNj0?rel=0&amp;controls=0&amp;modestbranding=1&amp;showinfo=0"></iframe>
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="http://opencart-demos.org/OPC07/OPC070174/image/cache/data/main_banner_1-940x420w.jpg" alt="slide1">
+                        <div class="carousel-caption">
+
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="http://opencart-demos.org/OPC07/OPC070174/image/cache/data/main_banner_1-940x420w.jpg" alt="slide2">
+                        <div class="carousel-caption">
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <!-- Controls -->
+                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                </a>
             </div>
         </div>
     <?php else: ?>
