dfe2884f63621a57e97f6233fc200094e35f0fd2
dungnt@smartosc.com <dungnt@smartosc.com>
2014-10-16 15:53:43 +0700

    fix conflict

--- application/languages/admin/en/lang.php
+++ application/languages/admin/en/lang.php
@@ -77,4 +77,33 @@ return array(
     'setting for contact'=>'Setting for contact',
     'update settings'=>'Update settings',
     'update'=>'Update',
+
+    //Games page
+    'games table'=> "Games Table",
+    'add new game'=> "Add New Game",
+    'games name'=> "Name",
+    'games desc'=> "Description",
+    'games image'=> "Images",
+    'edit game'=> "Edit Game",
+    'game name is not empty!'=> "Name is not empty!",
+    'Save Now'=> "Save Now",
+    'Reset'=> "Reset",
+
+
+
+    //Setting page in admin
+    'settings for website'=>'Settings For Website',
+    'general settings'=>'General Settings',
+    'group'=>'Group',
+    'action'=>'Action',
+    'active'=>'Enable',
+    'not active'=>'Disable',
+    'no data'=>'No Data',
+    'setting for home page'=>'Setting For Home Page',
+    'setting for the game'=>'Setting For The Game',
+    'setting for contact'=>'Setting For Contact',
+    'update status option success!'=>'Update Status Option Successful!',
+    'value'=>'Value',
+    'images'=>'Images',
+    'update settings'=>'Update Settings',
 );
