f2c053a39f76985358df5c3c5557b8c9de703d62
trangvt <trangvt@smartosc.com>
2014-10-24 17:16:35 +0700

    TrangVT add game_sort

--- application/modules/admin/controllers/GamesController.php
+++ application/modules/admin/controllers/GamesController.php
@@ -59,12 +59,15 @@ class Admin_GamesController extends FTeam_Controller_AdminAction
                 $game_desc = $request['game_desc'];
                 $game_status = $request['game_status'];
                 $game_difficult = $request['game_difficult'];
+                $game_sort = $request['game_sort'];
+
                 $game_name = $value['game_name'];
                 $singlegame = array(
                     'game_name' => $game_name,
                     'game_desc' => $game_desc,
                     'game_status' => $game_status,
-                    'game_difficult' =>$game_difficult
+                    'game_difficult' =>$game_difficult,
+                    'game_sort' =>$game_sort
                 );
                 $upload = new FTeam_UploadFile();
                 if ($upload->upload())
